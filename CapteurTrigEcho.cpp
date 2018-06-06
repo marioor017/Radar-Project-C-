eff332ef12e7714ca591a5e2e13d18fac6f1d71b
gabriel9449 <40001015+gabriel9449@users.noreply.github.com>
2018-06-06 19:37:08 +0200

    Update CapteurTrigEcho.cpp

--- CapteurTrigEcho.cpp
+++ CapteurTrigEcho.cpp
@@ -7,7 +7,8 @@
 
 #include "CapteurTrigEcho.h"
 
-CapteurTrigEcho::CapteurTrigEcho(int triggerPin, int echoPin) : CapteurDigital(echoPin) {
+CapteurTrigEcho::CapteurTrigEcho(int triggerPin, int echoPin) : CapteurDigital(echoPin) { //Ce constructeur utilise le constructeur de la classe mère
+	// (un capteur digital) et indique que c'est un port de sortie et envoi un 0 logique)
 	trigger = new Gpio(triggerPin);
 	trigger->dir(DIR_OUT);
 	trigger->write(0);
@@ -15,7 +16,7 @@ CapteurTrigEcho::CapteurTrigEcho(int triggerPin, int echoPin) : CapteurDigital(e
 
 }
 
-CapteurTrigEcho::~CapteurTrigEcho() {
+CapteurTrigEcho::~CapteurTrigEcho() { //Destructeur de la classe
 	// TODO Auto-generated destructor stub
 }
 
