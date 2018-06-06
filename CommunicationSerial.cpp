2319cff61afc0e22321f606587cf883ad4c87ec9
gabriel9449 <40001015+gabriel9449@users.noreply.github.com>
2018-06-06 19:40:14 +0200

    Update CommunicationSerial.cpp

--- CommunicationSerial.cpp
+++ CommunicationSerial.cpp
@@ -7,7 +7,7 @@
 
 #include "CommunicationSerial.h"
 
-CommunicationSerial::CommunicationSerial(int uartPort, int baudRate) {
+CommunicationSerial::CommunicationSerial(int uartPort, int baudRate) { 
 	uart = new mraa::Uart(uartPort); //Define the uart port to be used
  	uart->setBaudRate(baudRate); // Define the baudRate of the port
  	uart->setMode(8, mraa::UART_PARITY_NONE, 1); //Set the uart mode
@@ -16,10 +16,10 @@ CommunicationSerial::CommunicationSerial(int uartPort, int baudRate) {
 
 }
 
-CommunicationSerial::~CommunicationSerial() {
+CommunicationSerial::~CommunicationSerial() { //Destructeur de la classe
 	// TODO Auto-generated destructor stub
 }
-void CommunicationSerial::serialWrite(string msg){
+void CommunicationSerial::serialWrite(string msg){ //Cette méthode envoie un paquet de données dans le port serial 
 	uart->writeStr(msg);
 }
 
