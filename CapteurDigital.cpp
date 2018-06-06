a24e84ec4d254dc86adf1ade10baf6db99998dfc
gabriel9449 <40001015+gabriel9449@users.noreply.github.com>
2018-06-06 19:34:58 +0200

    Update CapteurDigital.cpp

--- CapteurDigital.cpp
+++ CapteurDigital.cpp
@@ -7,16 +7,17 @@
 
 #include "CapteurDigital.h"
 
-CapteurDigital::CapteurDigital(int port) : Capteur(port) {
+CapteurDigital::CapteurDigital(int port) : Capteur(port) {//Ce constructeur utilise le constructeur de la classe mère et 
+//initialise un port de type digitale au port indiqué
 	// TODO Auto-generated constructor stub
 	this->digital = new Gpio(port);
 	this->digital->dir(DIR_IN);
 }
 
-CapteurDigital::~CapteurDigital() {
+CapteurDigital::~CapteurDigital() { //Destructeur de la classe
 	// TODO Auto-generated destructor stub
 }
 
-int CapteurDigital::digitalRead(){
+int CapteurDigital::digitalRead(){ //Cette méthode realise une lecture du port digitale de l'objet
 	return this->digital->read();
 }
